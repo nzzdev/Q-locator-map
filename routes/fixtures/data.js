a231b58b90181446da35d9b5ccf2f416f25bead2
Manuel Roth <manuelroth@hotmail.ch>
2019-04-09 18:13:43 +0200

    Use the schema of q-map and add basic fixture data for point, linestring and polygon

--- routes/fixtures/data.js
+++ routes/fixtures/data.js
@@ -2,7 +2,11 @@ const fixtureDataDirectory = "../../resources/fixtures/data";
 
 // provide every fixture data file present in ../../resources/fixtures/data
 // has to be in sync with files created in build task - see ../../tasks/build.js
-const fixtureData = [require(`${fixtureDataDirectory}/basic.json`)];
+const fixtureData = [
+  require(`${fixtureDataDirectory}/point.json`),
+  require(`${fixtureDataDirectory}/linestring.json`),
+  require(`${fixtureDataDirectory}/polygon.json`)
+];
 
 module.exports = {
   path: "/fixtures/data",
