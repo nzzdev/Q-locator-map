4f77772b413685022b954579a68d426e0bdf881e
Manuel Roth <manuelroth@hotmail.ch>
2020-01-30 15:40:43 +0100

    Show abbreviated unit if scale is less than 75 pixel wide

--- scripts_src/scale.js
+++ scripts_src/scale.js
@@ -91,7 +91,7 @@ function setScale(container, maxWidth, maxDistance, unit) {
 
   const width = maxWidth * ratio;
   container.style.width = width + "px";
-  if (width > 70) {
+  if (width > 75) {
     container.innerHTML = distance + unit;
   } else {
     container.innerHTML = distance + shortUnit;
