ff20e9257f600ba269777921d9dcc6b17b457b39
Manuel Roth <manuelroth@hotmail.ch>
2020-10-28 19:22:07 +0100

    Add first implementation of mbtiles transfrom in tilesets service

--- tilesets/index.js
+++ tilesets/index.js
@@ -2,11 +2,17 @@ const fs = require("fs-extra");
 const util = require("util");
 const progress = require("progress-stream");
 const fetch = require("node-fetch");
+const tilelive = require("@mapbox/tilelive");
+const mbtiles = require("@mapbox/mbtiles");
 const unlink = util.promisify(fs.unlink);
 const path = require("path");
 const directory = process.env.ENV === "local" ? "./data/" : "/data/";
+const { getTransformStream, report } = require("./helpers.js");
 
-async function downloadTileset(url, path, size) {
+mbtiles.registerProtocols(tilelive);
+process.env.UV_THREADPOOL_SIZE = 4;
+
+async function downloadTileset(url, path, size, filename) {
   console.log(`Downloading ${url} to ${path}...`);
   await fs.ensureFile(path);
 
@@ -23,9 +29,27 @@ async function downloadTileset(url, path, size) {
       .pipe(progressStream)
       .pipe(fs.createWriteStream(path))
       .on("error", console.error)
-      .on("finish", () =>
-        console.log(`Download finished. Saved ${url} to ${path}.`)
-      );
+      .on("finish", () => {
+        console.log(`Download finished. Saved ${url} to ${path}.`);
+        // load the mbtiles source
+        tilelive.load(`mbtiles://./data/${filename}`, function (err, src) {
+          // load the s3 sink
+          tilelive.load(`mbtiles://./data/${filename}-transformed`, function (
+            err,
+            dst
+          ) {
+            const options = {
+              transform: getTransformStream(),
+              progress: report,
+              concurrency: 10,
+            }; // prepare options for tilelive copy
+            // now copy all tiles to the destination
+            tilelive.copy(src, dst, options, function (err) {
+              console.log("Transform finished.");
+            });
+          });
+        });
+      });
   }
 }
 
@@ -49,7 +73,12 @@ async function main() {
             tileset.url &&
             (isNFSMount || process.env.ENV === "local")
           ) {
-            await downloadTileset(tileset.url, tilesetPath, tileset.size);
+            await downloadTileset(
+              tileset.url,
+              tilesetPath,
+              tileset.size,
+              tileset.filename
+            );
           }
           if (
             !tileset.download &&
