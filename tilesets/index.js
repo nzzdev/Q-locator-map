e6691889f026710e2caeb98427f49c824a0487e0
Manuel Roth <manuelroth@hotmail.ch>
2020-11-05 17:32:28 +0100

    Improve transform process

--- tilesets/index.js
+++ tilesets/index.js
@@ -3,53 +3,66 @@ const util = require("util");
 const progress = require("progress-stream");
 const fetch = require("node-fetch");
 const tilelive = require("@mapbox/tilelive");
+const loadTileset = util.promisify(tilelive.load);
+const copyTileset = util.promisify(tilelive.copy);
 const mbtiles = require("@mapbox/mbtiles");
+const rmdir = util.promisify(fs.rmdir);
+const move = util.promisify(require("mv"));
 const unlink = util.promisify(fs.unlink);
 const path = require("path");
-const directory = process.env.ENV === "local" ? "./data/" : "/data/";
 const { getTransformStream, report } = require("./helpers.js");
 
 mbtiles.registerProtocols(tilelive);
-process.env.UV_THREADPOOL_SIZE = 4;
+process.env.UV_THREADPOOL_SIZE = 8;
 
-async function downloadTileset(url, path, size, filename) {
-  console.log(`Downloading ${url} to ${path}...`);
-  await fs.ensureFile(path);
+async function downloadTileset(url, path, size) {
+  try {
+    console.log(`Downloading ${url} to ${path}...`);
+    await fs.ensureFile(path);
 
-  const response = await fetch(url);
-  if (response.ok) {
-    const progressStream = progress({
-      length: size,
-      time: 1000 /* ms */,
-    });
-    progressStream.on("progress", (progress) => {
-      console.log(`Progress: ${Math.round(progress.percentage * 10) / 10} %\r`);
-    });
-    response.body
-      .pipe(progressStream)
-      .pipe(fs.createWriteStream(path))
-      .on("error", console.error)
-      .on("finish", () => {
-        console.log(`Download finished. Saved ${url} to ${path}.`);
-        // load the mbtiles source
-        tilelive.load(`mbtiles://./data/${filename}`, function (err, src) {
-          // load the s3 sink
-          tilelive.load(`mbtiles://./data/${filename}-transformed`, function (
-            err,
-            dst
-          ) {
-            const options = {
-              transform: getTransformStream(),
-              progress: report,
-              concurrency: 10,
-            }; // prepare options for tilelive copy
-            // now copy all tiles to the destination
-            tilelive.copy(src, dst, options, function (err) {
-              console.log("Transform finished.");
-            });
-          });
-        });
+    const response = await fetch(url);
+    if (response.ok) {
+      const progressStream = progress({
+        length: size,
+        time: 1000 /* ms */,
       });
+      progressStream.on("progress", (progress) => {
+        console.log(
+          `Progress: ${Math.round(progress.percentage * 10) / 10} %\r`
+        );
+      });
+      const result = await new Promise((resolve, reject) => {
+        response.body
+          .pipe(progressStream)
+          .pipe(fs.createWriteStream(path))
+          .on("error", (error) => reject(error))
+          .on("finish", () => {
+            resolve(`Download finished. Saved ${url} to ${path}.`);
+          });
+      });
+      console.log(result);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function transformTileset(path) {
+  try {
+    const sourceTileset = await loadTileset(`mbtiles://./${path}`);
+    const transformedTileset = await loadTileset(
+      `mbtiles://./${path}-transformed`
+    );
+
+    const options = {
+      transform: getTransformStream(),
+      progress: report,
+      concurrency: 10,
+    };
+    await copyTileset(sourceTileset, transformedTileset, options);
+    console.log("Transform finished.");
+  } catch (error) {
+    console.log(error);
   }
 }
 
@@ -62,23 +75,30 @@ async function deleteTileset(path) {
 async function main() {
   try {
     if (process.env.TILESETS) {
-      const tilesets = JSON.parse(process.env.TILESETS);
-      for (let tileset of Object.values(tilesets)) {
+      const tilesets = Object.values(JSON.parse(process.env.TILESETS));
+      for (let tileset of tilesets) {
         if (tileset.filename) {
+          let directory = "/data/";
+          if (process.env.ENV === "local") {
+            directory = "./data/";
+          }
+          if (tileset.transform) {
+            directory = "./transform/";
+            await rmdir(directory, { recursive: true });
+          }
           const tilesetPath = path.join(directory, tileset.filename);
-          // Container is mounted to NFS share if directory /data/.snapshot exists
-          const isNFSMount = fs.existsSync(path.join(directory, ".snapshot"));
-          if (
-            tileset.download &&
-            tileset.url &&
-            (isNFSMount || process.env.ENV === "local")
-          ) {
-            await downloadTileset(
-              tileset.url,
-              tilesetPath,
-              tileset.size,
-              tileset.filename
-            );
+          if (tileset.download && tileset.url) {
+            await downloadTileset(tileset.url, tilesetPath, tileset.size);
+          }
+
+          if (tileset.transform) {
+            await transformTileset(tilesetPath);
+            let destinationPath = `/data/${tileset.filename}`;
+            if (process.env.ENV === "local") {
+              destinationPath = `./data/${tileset.filename}`;
+            }
+            await move(`./${tilesetPath}-transformed`, destinationPath);
+            await rmdir(directory, { recursive: true });
           }
           if (
             !tileset.download &&
